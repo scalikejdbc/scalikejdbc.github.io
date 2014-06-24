c60837bb32c1e8771b9b6ff41d866c998f8fb357
Kazuhiro Sera <seratch@gmail.com>
2014-06-24 12:49:00 +0900

    Update for 2.0.3, 1.8.2

--- config.rb
+++ config.rb
@@ -62,10 +62,10 @@ set :markdown, fenced_code_blocks: true, smartypants: true, autolink: true, with
 I18n.enforce_available_locales = false
 
 # Latest Skinny Framework version
-@latest_version = "2.0.2"
+@latest_version = "2.0.3"
 @latest_play_support_version = "2.3.0"
 @v1_version = "1.7.7"
-@v18_version = "1.8.1"
+@v18_version = "1.8.2"
 
 set :version,        @latest_version
 set :latest_version, @latest_version
@@ -73,7 +73,7 @@ set :latest_play_support_version, @latest_play_support_version
 set :v1_version,        @v1_version
 set :v18_version,       @v18_version
 set :v1_latest_version, @v1_version
-set :h2_version,        "1.4.178"
+set :h2_version,        "1.4.179"
 set :logback_version,   "1.1.2"
 
 # Build-specific configuration
