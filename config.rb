4332d59793a06d3aaf9b4bbb5cc13586610f6e0a
Kazuhiro Sera <seratch@gmail.com>
2014-09-05 09:50:13 +0900

    2.1.1

--- config.rb
+++ config.rb
@@ -61,7 +61,7 @@ set :markdown, fenced_code_blocks: true, smartypants: true, autolink: true, with
 I18n.enforce_available_locales = false
 
 # Latest Skinny Framework version
-@latest_version = "2.1.0"
+@latest_version = "2.1.1"
 @latest_play_support_version = "2.3.1"
 @v1_version = "1.7.7"
 @v18_version = "1.8.2"
