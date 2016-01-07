a8dc5cbbbd5cc9bb2ec8ea608591363062cd6795
Kazuhiro Sera <seratch@gmail.com>
2016-01-08 08:52:14 +0900

    2.3.4

--- config.rb
+++ config.rb
@@ -61,7 +61,7 @@ set :markdown, fenced_code_blocks: true, smartypants: true, autolink: true, with
 I18n.enforce_available_locales = false
 
 # Latest versions
-@latest_version = "2.3.3"
+@latest_version = "2.3.4"
 @latest_play_support_version = "2.4.3"
 @v1_version = "1.7.7"
 @v18_version = "1.8.2"
