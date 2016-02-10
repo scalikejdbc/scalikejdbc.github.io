7b3c135213aefd263b6f8b0956c624bf9c6df22e
Kaz Sera <seratch@gmail.com>
2016-02-10 16:01:37 +0900

    2.3.5

--- config.rb
+++ config.rb
@@ -61,7 +61,7 @@ set :markdown, fenced_code_blocks: true, smartypants: true, autolink: true, with
 I18n.enforce_available_locales = false
 
 # Latest versions
-@latest_version = "2.3.4"
+@latest_version = "2.3.5"
 @latest_play_support_version = "2.4.3"
 @v1_version = "1.7.7"
 @v18_version = "1.8.2"
@@ -72,7 +72,7 @@ set :latest_play_support_version, @latest_play_support_version
 set :v1_version,        @v1_version
 set :v18_version,       @v18_version
 set :v1_latest_version, @v1_version
-set :h2_version,        "1.4.190"
+set :h2_version,        "1.4.191"
 set :logback_version,   "1.1.3"
 
 # Build-specific configuration
